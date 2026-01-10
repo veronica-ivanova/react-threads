8e5aacfb7dba4652fc0c2073c06f259b3c2c6529
veronica-ivanova <veronica.ivanova27@gmail.com>
2026-01-10 15:09:38 +0300

    feat: add auth page with login form

---
+++ src/features/login.tsx
@@ -0,0 +1,62 @@
+import React from "react"
+import { useForm } from "react-hook-form"
+import { Input } from "../components/input"
+import { Button, Link } from "@nextui-org/react"
+
+type Login = {
+  email: string
+  password: string
+}
+
+type Props = {
+  setSelected: (value: string) => void
+}
+
+export const Login: React.FC<Props> = ({ setSelected }) => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<Login>({
+    mode: "onChange",
+    reValidateMode: "onBlur",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+  return (
+    <form className="flex flex-col gap-4">
+      <Input
+        control={control}
+        name="email"
+        label="Email"
+        type="email"
+        required="Обязательное поле"
+      />
+      <Input
+        control={control}
+        name="password"
+        label="Пароль"
+        type="password"
+        required="Обязательное поле"
+      />
+
+      <p className="text-center text-small">
+        Нет аккаунта?{" "}
+        <Link
+          size="sm"
+          className="cursor-pointer"
+          onPress={() => setSelected("sign-up")}
+        >
+          Зарегистрируйтесь
+        </Link>
+      </p>
+      <div className="flex gap-2 justify-end">
+        <Button fullWidth color="primary" type="submit">
+          Войти
+        </Button>
+      </div>
+    </form>
+  )
+}

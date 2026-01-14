b9dabe0449013a5c7b7ebb2edb02a5204ca30088
veronica-ivanova <veronica.ivanova27@gmail.com>
2026-01-14 19:03:27 +0300

    feat: add create-post component and logout button

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react"
+import { useGetAllPostsQuery } from "../../app/services/postsApi"
+import { CreatePost } from "../../components/create-post"
 
 export const Posts = () => {
+  const { data } = useGetAllPostsQuery()
+
   return (
-    <div>
-      Posts
-    </div>
+    <>
+      <div className="mb-10 w-full">
+        <CreatePost />
+      </div>
+    </>
   )
 }

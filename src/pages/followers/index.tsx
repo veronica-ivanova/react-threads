afb6f8f99d5a91bee29b90699c6fce845878c848
veronica-ivanova <veronica.ivanova27@gmail.com>
2026-01-20 15:58:14 +0300

    feat: add followers and following pages

--- src/pages/followers/index.tsx
+++ src/pages/followers/index.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react"
+import { selectCurrent } from "../../features/user/userSlice"
+import { useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+import { Card, CardBody } from "@nextui-org/react"
+import { User } from "../../components/user"
 
 export const Followers = () => {
-  return (
-    <div>
-      Followers
+  const currentUser = useSelector(selectCurrent)
+
+  if (!currentUser) {
+    return null
+  }
+
+  return currentUser.followers.length > 0 ? (
+    <div className="gap-5 flex flex-col">
+      {currentUser.followers.map(user => (
+        <Link to={`/users/${user.follower.id}`} key={user.follower.id}>
+          <Card>
+            <CardBody className="block">
+              <User
+                name={user.follower.name ?? ""}
+                avatarUrl={user.follower.avatarUrl ?? ""}
+                description={user.follower.email}
+              />
+            </CardBody>
+          </Card>
+        </Link>
+      ))}
     </div>
+  ) : (
+    <h2>У вас нет подписчиков</h2>
   )
 }
--- src/pages/following/index.tsx
+++ src/pages/following/index.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react"
+import { selectCurrent } from "../../features/user/userSlice"
+import { useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+import { Card, CardBody } from "@nextui-org/react"
+import { User } from "../../components/user"
 
 export const Following = () => {
-  return (
-    <div>
-      Following
+  const currentUser = useSelector(selectCurrent)
+
+  if (!currentUser) {
+    return null
+  }
+
+  return currentUser.following.length > 0 ? (
+    <div className="gap-5 flex flex-col">
+      {currentUser.following.map(user => (
+        <Link to={`/users/${user.following.id}`} key={user.following.id}>
+          <Card>
+            <CardBody className="block">
+              <User
+                name={user.following.name ?? ""}
+                avatarUrl={user.following.avatarUrl ?? ""}
+                description={user.following.email}
+              />
+            </CardBody>
+          </Card>
+        </Link>
+      ))}
     </div>
+  ) : (
+    <h2>У вас нет подписчиков</h2>
   )
 }
